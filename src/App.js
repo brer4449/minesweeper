e6ac667db6fe325326b1cb39261fee22b821fc0c
Brendan Erickson <berickson92@Gmail.com>
2024-07-27 22:34:28 -0400

    removed unnecessary files and added background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const backgroundStyles = {   
+    backgroundImage: `url(https://st2.depositphotos.com/1526816/6712/v/450/depositphotos_67129419-stock-illustration-forest.jpg)`,
+    height: "100vh",
+    width: "auto",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={backgroundStyles}>
+        <h1>hello world</h1>
     </div>
   );
 }
